416cee12376caa5583b4e6c3c08e990ecf62d53b
Luba <bart6891@ya.ru>
2022-09-30 16:35:15 +0300

    IfElse

---
+++ example0005_hellouser/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "люба")
+{
+   Console.WriteLine("Ура, это же Люба!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
